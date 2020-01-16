6504d53f4e901e66bbf4eb4975ef3af98dd79aa0
Daniel <idaniel.com1@gmail.com>
2020-01-16 17:35:32 +0200

    фильтр по разным параметрам

--- src/groups/views.py
+++ src/groups/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.db.models import Q
 
 from groups.models import Group
 from groups.forms import GroupsAddForm
@@ -16,7 +17,9 @@ def groups(request):
 
     fn = request.GET.get('group_name')
     if fn:
-        queryset = queryset.filter(group_name__istartswith=fn)
+        queryset = queryset.filter(group_name__istartswith=fn |
+                                   Q(group_size__icontains=fn) |
+                                   Q(group_email__icontains=fn))
 
     for group in queryset:
         response += group.get_info() + '<br>'
