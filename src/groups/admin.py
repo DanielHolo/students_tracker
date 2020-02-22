1a7ec9722bd7a2cf9fcfd834e520b171aed6f147
Daniel <idaniel.com1@gmail.com>
2020-02-22 18:29:38 +0200

    lesson 23_01 homework

--- src/groups/admin.py
+++ src/groups/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from groups.models import Group
+from students.models import Student
+
+
+class GroupAdmin(admin.ModelAdmin):
+    pass
+
+
+class StudentInline(admin.TabularInline):
+    model = Student
+
+
+class GroupAdmin(admin.ModelAdmin):
+    inlines = (StudentInline,)
+
+
+admin.site.register(Group, GroupAdmin)
--- src/teachers/admin.py
+++ src/teachers/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from teachers.models import Teacher
+
+
+class TeacherAdmin(admin.ModelAdmin):
+    readonly_fields = ('email', 'telephone')
+    list_display = ('id', 'first_name', 'last_name', 'email', 'group')
+    list_select_related = ('group',)
+
+
+admin.site.register(Teacher, TeacherAdmin)
